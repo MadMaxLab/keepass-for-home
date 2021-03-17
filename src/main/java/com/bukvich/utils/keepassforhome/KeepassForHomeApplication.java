41dd703e773179dc0fab914bba6a7b260a05f733
Maxim Bukvich <bukvich.m.ni@sberbank.ru>
2021-03-17 12:33:54 +0300

    Initial commit

---
+++ src/main/java/com/bukvich/utils/keepassforhome/KeepassForHomeApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Maxim Bukvich
+ */
+
+package com.bukvich.utils.keepassforhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepassForHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepassForHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bukvich/utils/keepassforhome/KeepassForHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bukvich.utils.keepassforhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepassForHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
